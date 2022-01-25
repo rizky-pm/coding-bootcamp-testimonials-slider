649c91056ee844911ed655bd22e4152fe4bdc68f
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2022-01-25 08:46:59 +0700

    Fixed some bugs

--- app/js/app.js
+++ app/js/app.js
@@ -3,13 +3,13 @@ const testimonials = [
         review: '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”',
         name: 'Tanya Sinclair',
         job: 'UX Engineer',
-        picture: '../../images/image-tanya.jpg',
+        picture: './images/image-tanya.jpg',
     },
     {
         review: '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”',
         name: 'John Tarkpor',
         job: 'Junior Front-end Developer',
-        picture: '../../images/image-john.jpg',
+        picture: './images/image-john.jpg',
     },
 ];
 
