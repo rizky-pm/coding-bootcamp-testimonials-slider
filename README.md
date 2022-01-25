efc9e76d3594dfa39773264bb182f306d64899f7
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2022-01-25 08:43:05 +0700

    Completed mobile and desktop view

---
+++ README.md
@@ -0,0 +1,47 @@
+# Frontend Mentor - Coding bootcamp testimonials slider solution
+
+This is a solution to the [Coding bootcamp testimonials slider challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/coding-bootcamp-testimonials-slider-4FNyLA8JL). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+-   [Overview](#overview)
+    -   [The challenge](#the-challenge)
+    -   [Screenshot](#screenshot)
+    -   [Links](#links)
+-   [My process](#my-process)
+    -   [Built with](#built-with)
+-   [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+-   View the optimal layout for the component depending on their device's screen size
+-   Navigate the slider using either their mouse/trackpad or keyboard
+
+### Screenshot
+
+![Desktop](./images/desktop-design-slide-1.jpg)
+![Mobile](./images/mobile-design-slide-2.jpg)
+
+### Links
+
+-   Solution URL: [Github](https://your-solution-url.com)
+-   Live Site URL: [Github Pages](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+-   Semantic HTML5 markup
+-   CSS custom properties
+-   Flexbox
+-   CSS Grid
+-   Mobile-first workflow
+
+## Author
+
+-   Frontend Mentor - [@rizky-pm](https://www.frontendmentor.io/profile/rizky-pm)
+-   Github - [@rizky-pm](https://github.com/rizky-pm)
---
+++ app/js/app.js
@@ -0,0 +1,57 @@
+const testimonials = [
+    {
+        review: '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”',
+        name: 'Tanya Sinclair',
+        job: 'UX Engineer',
+        picture: '../../images/image-tanya.jpg',
+    },
+    {
+        review: '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”',
+        name: 'John Tarkpor',
+        job: 'Junior Front-end Developer',
+        picture: '../../images/image-john.jpg',
+    },
+];
+
+const profilePicture = $('.profile-picture');
+const textReview = $('.text__review');
+const textName = $('.text__name');
+const textJob = $('.text__job');
+const buttonPrev = $('.button__prev');
+const buttonNext = $('.button__next');
+let index = 0;
+// ---------------------------------------------------------------
+const injectData = (i) => {
+    profilePicture.attr('src', testimonials[i].picture);
+    textReview.text(testimonials[i].review);
+    textName.text(testimonials[i].name);
+    textJob.text(testimonials[i].job);
+};
+
+$(document).ready(function () {
+    injectData(index);
+});
+
+buttonNext.click(function (e) {
+    e.preventDefault();
+
+    if (index === 0) {
+        index = 1;
+        injectData(index);
+    } else {
+        index = 0;
+        injectData(index);
+    }
+});
+
+buttonPrev.click((e) => {
+    e.preventDefault();
+
+    if (index === 1) {
+        index = 0;
+        injectData(index);
+    } else {
+        index = 1;
+        injectData(index);
+    }
+});
